b5c9c8a86a1a4297d432941ef4f63da7f68591f9
chenjiefei <nlpx_dc@hotmail.com>
2018-06-24 23:59:38 +0800

    first init

---
+++ src/main/java/com/xiaofei/blogsite/BlogsiteApplication.java
@@ -0,0 +1,12 @@
+package com.xiaofei.blogsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsiteApplication.class, args);
+    }
+}
